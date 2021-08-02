2a71ac89bf356b5fe27df11f2fd700bae2970c6d
braankoo <lozinka007>
2021-08-02 19:09:59 +0200

    change number of campaign ids passed to the get adgroup request

--- app/Jobs/GetAdGroups.php
+++ app/Jobs/GetAdGroups.php
@@ -53,7 +53,7 @@ class GetAdGroups implements ShouldQueue {
                 $this->fetchAndStore([]);
                 break;
             case 'google':
-                $this->client->campaigns()->chunk(100, function ($campaigns) {
+                $this->client->campaigns()->chunk(50, function ($campaigns) {
                     $this->fetchAndStore($campaigns->map->id->toArray());
                 });
                 break;
