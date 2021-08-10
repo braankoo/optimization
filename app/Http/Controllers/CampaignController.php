95d58873ba263db4450a252a5acc0ef93d2a215b
braankoo <lozinka007>
2021-08-10 15:09:27 +0200

    .

--- app/Http/Controllers/CampaignController.php
+++ app/Http/Controllers/CampaignController.php
@@ -65,8 +65,45 @@ class CampaignController extends Controller {
             ->groupBy('ad_groups.id')
             ->paginate(50);
 
+        $total = DB::connection($adPlatform)->table('ad_groups')->selectRaw(
+            "
+             ad_groups.name as name,
+             ad_groups.id as id,
+             sum(clicks) as clicks,
+             sum(impressions) as impressions,
+             TRUNCATE((sum(cost) / 1000000),2) as cost,
+             TRUNCATE((sum(earned) / 1000000),2) as earned,
+             TRUNCATE(( sum(cost) / 1000000 ) / sum(profile),2) as actual_cpa,
+             TRUNCATE(( sum(cost) / 1000000 ) / sum(upgrade),2)   as actual_cps,
+             TRUNCATE(((sum(cost) / sum(clicks)) / 1000000),2) as avg_cpc,
+             TRUNCATE(( (sum(earned) / 1000000) - ( sum(cost) / 1000000) ),2) as pl,
+             IFNULL(TRUNCATE(SUM(`impressions` * `avg_position`) / SUM(impressions),1),0) as avg_position,
+             IFNULL(ROUND( (sum(profile) / sum(upgrade)) ),0) as ur,
+             IFNULL(TRUNCATE((( (sum(earned) / 1000000) -  ( sum(cost) / 1000000 ) ) / (sum(cost) / 1000000)) * 100,2),0) as roi,
+             IFNULL(ROUND((sum(earned) / 1000000) / sum(profile),2),0) as actual_epa,
+             IFNULL(ROUND((sum(earned) / 1000000) / sum(upgrade),2),0) as actual_eps,
+             IFNULL(ROUND((sum(profile) / sum(clicks) * 100),2),0) as cr,
+             IFNULL(ROUND( (sum(clicks) / sum(impressions) * 100 ),2),0) as ctr,
+             sum(profile) as profiles,
+             sum(upgrade) as upgrades,
+             status")
+            ->join('stats_ad_groups', 'ad_groups.id', '=', 'stats_ad_groups.ad_group_id')
+            ->where('ad_groups.campaign_id', '=', $campaign->id)
+            ->when(!empty($request->input('name')), function ($q) use ($request) {
+                $q->where('name', 'LIKE', "%" . $request->input('name') . "%");
+            })
+            ->when(!empty($request->input('startDate')), function ($q) use ($request) {
+                $q->whereDate('created_at', '>=', $request->input('startDate'));
+            })
+            ->when(!empty($request->input('startDate')), function ($q) use ($request) {
+                $q->whereDate('created_at', '>=', $request->input('endDate'));
+            })
+            ->orderBy(!empty($request->input('sortBy')) ? $request->input('sortBy') : 'name', $request->input('sortDesc') == 'true' ? 'ASC' : 'DESC')
+            ->groupBy('ad_groups.id')
+            ->get();
 
-        $total = $this->prepareTotals(collect($data->items()));
+
+        $total = $this->prepareTotals(collect($total));
 
 
         return response()->json(
--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -60,7 +60,42 @@ class ClientController extends Controller {
             ->paginate(50);
 
 
-        $total = $this->prepareTotals(collect($data->items()));
+        $total = DB::connection($adPlatform)->table('clients')->selectRaw(
+            "
+             clients.name as name,
+             clients.id as id,
+             sum(clicks) as clicks,
+             sum(impressions) as impressions,
+             ROUND(TRUNCATE((sum(cost) / 1000000),2),2) as cost,
+             ROUND(TRUNCATE((sum(earned) / 1000000),2),2) as earned,
+             ROUND(TRUNCATE(( sum(cost) / 1000000 ) / sum(profile),2),2) as actual_cpa,
+             TRUNCATE(( sum(cost) / 1000000 ) / sum(upgrade),2)   as actual_cps,
+             IFNULL(TRUNCATE(((sum(cost) / sum(clicks)) / 1000000),2),0) as avg_cpc,
+             ROUND(TRUNCATE(( (sum(earned) / 1000000) - ( sum(cost) / 1000000) ),2),2) as pl,
+             IFNULL(TRUNCATE(SUM(`impressions` * `avg_position`) / SUM(impressions),1),0) as avg_position,
+             IFNULL(ROUND( (sum(profile) / sum(upgrade)) ),0) as ur,
+             IFNULL(TRUNCATE((( (sum(earned) / 1000000) -  ( sum(cost) / 1000000 ) ) / (sum(cost) / 1000000)) * 100,2),0) as roi,
+             IFNULL(TRUNCATE((sum(earned) / 1000000) / sum(profile),2),0) as actual_epa,
+             IFNULL(TRUNCATE((sum(earned) / 1000000) / sum(upgrade),2),0) as actual_eps,
+             IFNULL(TRUNCATE((sum(profile) / sum(clicks) * 100),2),0) as cr,
+             IFNULL(TRUNCATE( (sum(clicks) / sum(impressions) * 100 ),2),0) as ctr,
+             sum(profile) as profiles,
+             sum(upgrade) as upgrades")
+            ->join('stats_clients', 'clients.id', '=', 'stats_clients.client_id')
+            ->when(!empty($request->input('name')), function ($q) use ($request) {
+                $q->where('name', 'LIKE', "%" . $request->input('name') . "%");
+            })
+            ->when(!empty($request->input('startDate')), function ($q) use ($request) {
+                $q->whereDate('created_at', '>=', $request->input('startDate'));
+            })
+            ->when(!empty($request->input('endDate')), function ($q) use ($request) {
+                $q->whereDate('created_at', '<=', $request->input('endDate'));
+            })
+            ->groupBy('client_id')
+            ->orderBy(!empty($request->input('sortBy')) ? $request->input('sortBy') : 'name', $request->input('sortDesc') == 'true' ? 'ASC' : 'DESC')
+            ->get();
+
+        $total = $this->prepareTotals(collect($total));
 
 
         return response()->json(
@@ -120,7 +155,44 @@ class ClientController extends Controller {
             ->paginate(50);
 
 
-        $total = $this->prepareTotals(collect($data->items()));
+        $total = DB::connection($adPlatform)->table('campaigns')->selectRaw(
+            "
+             campaigns.name as name,
+             campaigns.id as id,
+             sum(clicks) as clicks,
+             sum(impressions) as impressions,
+             ROUND(TRUNCATE((sum(cost) / 1000000),2),2) as cost,
+             TRUNCATE((sum(earned) / 1000000),2) as earned,
+             TRUNCATE(( sum(cost) / 1000000 ) / sum(profile),2) as actual_cpa,
+             TRUNCATE(( sum(cost) / 1000000 ) / sum(upgrade),2)   as actual_cps,
+             TRUNCATE(((sum(cost) / sum(clicks)) / 1000000),2) as avg_cpc,
+             TRUNCATE(( (sum(earned) / 1000000) - ( sum(cost) / 1000000) ),2) as pl,
+             IFNULL(TRUNCATE(SUM(`impressions` * `avg_position`) / SUM(impressions),1),0) as avg_position,
+             IFNULL(ROUND( (sum(profile) / sum(upgrade)) ),0) as ur,
+             IFNULL(TRUNCATE((( (sum(earned) / 1000000) -  ( sum(cost) / 1000000 ) ) / (sum(cost) / 1000000)) * 100,2),0) as roi,
+             IFNULL(ROUND((sum(earned) / 1000000) / sum(profile),2),0) as actual_epa,
+             IFNULL(ROUND((sum(earned) / 1000000) / sum(upgrade),2),0) as actual_eps,
+             IFNULL(ROUND((sum(profile) / sum(clicks) * 100),2),0) as cr,
+             IFNULL(ROUND( (sum(clicks) / sum(impressions) * 100 ),2),0) as ctr,
+             sum(profile) as profiles,
+             sum(upgrade) as upgrades")
+            ->join('stats_campaigns', 'campaigns.id', '=', 'stats_campaigns.campaign_id')
+            ->where('campaigns.client_id', '=', $client->id)
+            ->when(!empty($request->input('name')), function ($q) use ($request) {
+                $q->where('name', 'LIKE', "%" . $request->input('name') . "%");
+            })
+            ->when(!empty($request->input('startDate')), function ($q) use ($request) {
+                $q->whereDate('created_at', '>=', $request->input('startDate'));
+            })
+            ->when(!empty($request->input('endDate')), function ($q) use ($request) {
+                $q->whereDate('created_at', '<=', $request->input('endDate'));
+            })
+            ->orderBy(!empty($request->input('sortBy')) ? $request->input('sortBy') : 'name', $request->input('sortDesc') == 'true' ? 'ASC' : 'DESC')
+            ->groupBy('campaigns.id')
+            ->get();
+
+
+        $total = $this->prepareTotals(collect($total));
 
 
         return response()->json(
