f25909765db6a1624b4a2e7c3bf18ff5569f58b3
Branko Dragovic <braankoo@gmail.com>
2022-01-21 18:13:02 +0100

    fix kernel typo

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -36,7 +36,7 @@ class Kernel extends ConsoleKernel {
     {
         foreach ( [ 'google', 'bing', 'gemini' ] as $platform )
         {
-            $schedule->command('prepare:date', [ $platform, '--startDate' => Carbon::now()->format('Y-m-d') ])->everyFourHours();
+            $schedule->command('prepare:data', [ $platform, '--startDate' => Carbon::now()->format('Y-m-d') ])->everyFourHours();
         }
 
     }
